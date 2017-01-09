736da09f9d4b97ad40d4b6aece06f4669d17886a
Rafael de Paula Herrera <herrera.rp@gmail.com>
2017-01-09 17:26:56 -0200

    janitor process just added

--- Dockerfile
+++ Dockerfile
@@ -2,12 +2,14 @@ FROM python:2.7.13-alpine
 MAINTAINER Rafael de Paula Herrera <herrera.rp@gmail.com>
 
 RUN apk add --update py-pip && \
-    pip install boto3 && \
     rm -rf /var/cache/apk/* && \
+    pip install boto3 && \
     mkdir /backup-ebs-volumes
 
 WORKDIR /backup-ebs-volumes
 
 COPY backup-ebs-volumes.py /backup-ebs-volumes/backup-ebs-volumes.py
+COPY ebs-snapshot-janitor.py /backup-ebs-volumes/ebs-snapshot-janitor.py
 
-CMD ["python", "backup-ebs-volumes.py"]
+CMD ["python"]
+ENTRYPOINT ["backup-ebs-volumes.py"]
--- backup-ebs-volumes.py
+++ backup-ebs-volumes.py
@@ -2,19 +2,24 @@
 # -*- coding: utf-8 -*-
 
 #
-# This example was taken from:
+# Taken from:
 # https://serverlesscode.com/post/lambda-schedule-ebs-snapshot-backups/
 #
 
 import boto3
+import collections
+import datetime
 
-if __name__ == '__main__':
-    ec = boto3.client('ec2')
+ec = boto3.client('ec2')
+
+def lambda_handler(event, context):
     reservations = ec.describe_instances(
-            Filters=[
-                {'Name': 'tag-key', 'Values': ['backup', 'Backup']},
-            ]
-        )['Reservations']
+        Filters=[
+            {'Name': 'tag-key', 'Values': ['backup', 'Backup']},
+        ]
+    ).get(
+        'Reservations', []
+    )
 
     instances = sum(
         [
@@ -22,15 +27,46 @@ if __name__ == '__main__':
             for r in reservations
         ], [])
 
+    print "Found %d instances that need backing up" % len(instances)
+
+    to_tag = collections.defaultdict(list)
+
     for instance in instances:
+        try:
+            retention_days = [
+                int(t.get('Value')) for t in instance['Tags']
+                if t['Key'] == 'Retention'][0]
+        except IndexError:
+            retention_days = 7
+
         for dev in instance['BlockDeviceMappings']:
             if dev.get('Ebs', None) is None:
-                # skip non-EBS volumes
                 continue
             vol_id = dev['Ebs']['VolumeId']
             print "Found EBS volume %s on instance %s" % (
                 vol_id, instance['InstanceId'])
 
-            ec.create_snapshot(
+            snap = ec.create_snapshot(
                 VolumeId=vol_id,
             )
+
+            to_tag[retention_days].append(snap['SnapshotId'])
+
+            print "Retaining snapshot %s of volume %s from instance %s for %d days" % (
+                snap['SnapshotId'],
+                vol_id,
+                instance['InstanceId'],
+                retention_days,
+            )
+
+
+    for retention_days in to_tag.keys():
+        delete_date = datetime.date.today() + datetime.timedelta(days=retention_days)
+        delete_fmt = delete_date.strftime('%Y-%m-%d')
+        print "Will delete %d snapshots on %s" % (len(to_tag[retention_days]), delete_fmt)
+        ec.create_tags(
+            Resources=to_tag[retention_days],
+            Tags=[
+                {'Key': 'DeleteOn', 'Value': delete_fmt},
+            ]
+        )
